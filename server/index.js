f6f4a39d0fc155bffb5c481e19c4196b02508d44
MeerKatnip <amy.roy@icloud.com>
2021-01-17 13:23:01 -0600

    setup of the folders and installing dependencies

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 4090;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
